057c40c67eaec487e6c37551bd7a4953f0f80c05
Caleb Knox <knoxcaleb@aol.com>
2016-06-12 14:04:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,16 @@
 class Dog
-  @@all = []
-
   attr_accessor :name
-
+  @@all = []
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
-
   def self.clear_all
     @@all.clear
+  end
+
+  def self.all
+    @@all.each {|dog| puts "#{dog.name}"}
   end
 end
